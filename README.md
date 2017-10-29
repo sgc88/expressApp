0a5eb3f2d29a7980c440f8df95bd88bad0661351
sgc88 <sehribancheng@yahoo.com>
2017-10-29 16:12:34 -0700

    app.js ile public dosyalarini iliskilendirdik

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # expressApp
+
+bu app express icin ilk adimdir.
+app.use(express.static('public')) => middleware; app.js ile kullanici tarafindaki dosyalari birbiriyle iliskilendirir.
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var  express = require('express');
 
 var app = express(); // yeni app yaptigimizda her zaman ilk kodumuz bu olur
+
+//middleware
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
-  res.sendFile('views/index.html', {root: __dirname}); // bu sekilde html endpint kullanmis oluyoruz 
+  res.sendFile('views/index.html', {root: __dirname}); // bu sekilde html endpint kullanmis oluyoruz
 });
 
 app.listen(3000, function(){
